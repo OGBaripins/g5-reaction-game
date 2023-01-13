b611abcf7a273a58a9a0fb683cd857f83baa60b3
Aleksandrs Barbins <bbarbins@gmail.com>
2023-01-13 08:47:25 +0200

    Added sum thing but mostly broke things

--- app/src/main/java/com/example/reaction_game/LoginActivity.java
+++ app/src/main/java/com/example/reaction_game/LoginActivity.java
@@ -1,14 +1,28 @@
 package com.example.reaction_game;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
+        final View rootView = inflater.inflate(R.layout.activity_login, container, false);
+
+        ImageView back = ((ImageView) rootView.findViewById(R.id.back_btn));
+        back.setOnClickListener(v -> {
+            Intent myIntent = new Intent(this, EntryActivity.class);
+            myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(myIntent);
+        });
     }
 }
